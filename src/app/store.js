832d408f308ef818584a100a1a969df3541c6591
deJames <109223910+deJames-13@users.noreply.github.com>
2024-08-18 16:07:35 +0800

    start

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { apiSlice } from '@config/api';
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import rootReducer from './reducers.js';
+
 const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: import.meta.env.VITE_CLIENT_ENV === 'development',
 });
 setupListeners(store.dispatch);
+
 export default store;
