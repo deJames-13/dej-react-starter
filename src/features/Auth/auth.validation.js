f4cbd405e0ba7db8311fdce6f507cdfa6cc6f82e
deJames <109223910+deJames-13@users.noreply.github.com>
2024-08-20 16:55:35 +0800

    a

--- src/features/Auth/auth.validation.js
+++ src/features/Auth/auth.validation.js
@@ -1,4 +1,5 @@
 import * as yup from 'yup';
+
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 export const signupValidation = yup.object().shape({
   username: yup.string().min(3, 'Username must be at least 3 characters long').required('*Required'),
